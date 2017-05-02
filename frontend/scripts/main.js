64205bf2bddf730402f97044d83306777c17ab79
xiangc2 <850413556@qq.com>
2017-05-01 22:16:52 -0500

    Add files via upload

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -30,6 +30,8 @@ function initMap() {
 
         	modal.style.display = "block";
         });
+
+
         
 
     	load_markers();
@@ -39,7 +41,7 @@ function initMap() {
     });
 }
 
-span.onclick = function() {
+function close_popWindow() {
     modal.style.display = "none";
 }
 
@@ -83,7 +85,7 @@ function submit_marker() {
             // do some logging
         }
     });
-
+    close_popWindow();
     reload_markers();
     
     return false;
@@ -103,19 +105,31 @@ function callback(data) {
         console.log(marker_pos);
     
         var contentString = 
-        
-        '<h1 id="info_title">'+marker.title+'</h1>'+
-        '<p id="info_type">Type: '+marker.type+'</p>'+
-        '<p id="info_time">Time created: '+marker.time+'</p>'+
-
-        '<p id="info_message">    '+marker.message+'</p>' ;
+                    '<div id="iw-container">' +
+                    '<div class="iw-title">'+marker.title+'</div>' +
+                    '<div class="iw-content">' +
+                        '<div class="infowindow">'+
+                        '<ul>'+
+                            '<li><a id="smalltitle">Type:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </a></li>'+
+                            '<li><a>'+marker.type+'</a></li>'+
+                        '</ul>'+
+                        '<ul>'+
+                            '<li><a id="smalltitle">Time created:</a></li>'+
+                            '<li><a>'+marker.time+'</a></li>'+
+                        '</ul>'+
+                        '<p id="infocontent">'+marker.message+'</p>'+
+                        '</div>'+
+                    '</div>' +
+                    '<div class="iw-bottom-gradient"></div>' +
+                    '</div>';
 
         var myinfowindow = new google.maps.InfoWindow({
             content:contentString,
-            maxWidth: 210
+            maxWidth: 350
         });
 
         var map_marker = new google.maps.Marker( {
+            id:marker._id,
             position: marker_pos,
             map: map,
             title: marker.title,
@@ -131,6 +145,56 @@ function callback(data) {
         google.maps.event.addListener(map_marker, 'click', function() {
             this.infowindow.open(map, this);
         });
+
+        google.maps.event.addListener(map_marker,'rightclick',function(){
+            delete_marker(this.id);
+        });
+
+        google.maps.event.addListener(myinfowindow, 'domready', function() {
+    
+    // Reference to the DIV that wraps the bottom of infowindow
+    var iwOuter = $('.gm-style-iw');
+
+    /* Since this div is in a position prior to .gm-div style-iw.
+     * We use jQuery and create a iwBackground variable,
+     * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
+    */
+    var iwBackground = iwOuter.prev();
+
+    // Removes background shadow DIV
+    iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+    // Removes white background DIV
+    iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+    // Moves the infowindow 115px to the right.
+    iwOuter.parent().parent().css({left: '115px'});
+
+    // Moves the shadow of the arrow 76px to the left margin.
+    iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+    // Moves the arrow 76px to the left margin.
+    iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+    // Changes the desired tail shadow color.
+    iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
+
+    // Reference to the div that groups the close button elements.
+    var iwCloseBtn = iwOuter.next();
+
+    // Apply the desired effect to the close button
+    iwCloseBtn.css({opacity: '1', right: '38px', top: '3px', border: '7px solid #48b5e9', 'border-radius': '13px', 'box-shadow': '0 0 5px #3990B9'});
+
+    // If the content of infowindow not exceed the set maximum height, then the gradient is removed.
+    if($('.iw-content').height() < 140){
+      $('.iw-bottom-gradient').css({display: 'none'});
+    }
+
+    // The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
+    iwCloseBtn.mouseout(function(){
+      $(this).css({opacity: '1'});
+    });
+  });
 
         
     }    
@@ -157,13 +221,39 @@ function addMarker(location) {
         position: location,
         map: map
     });
-/*    
-    $.ajax({
-        type: "POST",
-        url: 'http://localhost:8080/api/markers',
-        data: !!!!!
-        success: post_callback
-});
-*/
 
 }
+
+function load_type(type){
+    clear_markers();
+    $.ajax({
+        url: 'http://localhost:8080/api/markers/search?type='+type,
+        type: 'GET',
+        success: callback
+    }); 
+}
+
+function show_all(){
+    clear_markers();
+    reload_markers();
+}
+
+function adjust_textarea(h) {
+    h.style.height = "20px";
+    h.style.height = (h.scrollHeight)+"px";
+}
+
+function delete_marker(id){
+    $.ajax({
+        url: 'http://localhost:8080/api/markers/'+id,
+        type: 'delete',
+        success: delete_callback
+    }); 
+    clear_markers();
+    reload_markers();
+}
+
+function delete_callback(data){
+    console.log("delete success");
+}
+
